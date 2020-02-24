6eb5f3cafa29565985c0eb5907bdb8ff0996954b
Vladislav Novak <novak.vlad97@mail.ru>
2020-02-24 22:08:14 +0300

    Updates

---
+++ precache-manifest.ccc25e0bec4e9aba8f2c1f11b8eee03b.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "13f6a7bb661a91d26cd7e42148303811",
+    "url": "/Portfolio/index.html"
+  },
+  {
+    "revision": "fb65a034b393aeaa07ad",
+    "url": "/Portfolio/static/css/2.17e5ed98.chunk.css"
+  },
+  {
+    "revision": "ef9e94410b5a96fa9f88",
+    "url": "/Portfolio/static/css/main.d255163c.chunk.css"
+  },
+  {
+    "revision": "fb65a034b393aeaa07ad",
+    "url": "/Portfolio/static/js/2.79914e85.chunk.js"
+  },
+  {
+    "revision": "f032203ca460334c00de541c30a6078a",
+    "url": "/Portfolio/static/js/2.79914e85.chunk.js.LICENSE"
+  },
+  {
+    "revision": "ef9e94410b5a96fa9f88",
+    "url": "/Portfolio/static/js/main.d4730d87.chunk.js"
+  },
+  {
+    "revision": "fc5338d523801bf5e718",
+    "url": "/Portfolio/static/js/runtime-main.4005ac6c.js"
+  },
+  {
+    "revision": "b1c5eac719690426b4e41d2701db4e88",
+    "url": "/Portfolio/static/media/aprtm_plnr1.b1c5eac7.jpg"
+  },
+  {
+    "revision": "617119bd29f6e76d8c12a0f7e574221f",
+    "url": "/Portfolio/static/media/aprtm_plnr2.617119bd.jpg"
+  },
+  {
+    "revision": "eea1c9891e5b1b9f7eed33c986133824",
+    "url": "/Portfolio/static/media/aprtm_plnr3.eea1c989.jpg"
+  },
+  {
+    "revision": "2157635935ef7c1d268b68ff2843c3d0",
+    "url": "/Portfolio/static/media/aprtm_plnr4.21576359.jpg"
+  }
+]);
