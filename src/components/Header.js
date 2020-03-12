4dd140147a5b9a24e0409c7682e49d121a0bf6a6
Vladislav Novak <novak.vlad97@mail.ru>
2020-03-12 23:31:11 +0300

    Added resume pdf file.

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,8 +3,20 @@ import { Link } from 'react-scroll';
 import DropdownButton from 'react-bootstrap/DropdownButton';
 import Dropdown from 'react-bootstrap/Dropdown';
 
+//google analytics
+import ReactGA from 'react-ga';
+//resume pdf
+import resume_pdf from '../resume_Novak_Vladislav.pdf';
+
 class Header extends Component {
     render () {
+        function ResumeClick() {
+            ReactGA.event({
+                category: 'Resume',
+                action: 'Click resume pdf file'
+            })
+        }
+
         return (
             <div className="navWrap" id="aboutme">
                 <header className="header">
@@ -14,6 +26,7 @@ class Header extends Component {
                         <Link activeClass="active" className="menuItem" to="aboutme" spy={true} smooth={true} duration={500}>about me</Link>
                         <Link activeClass="active" className="menuItem" to="projects" spy={true} smooth={true} duration={500}>projects</Link>
                         <Link activeClass="active" className="menuItem" to="footer" spy={true} smooth={true} duration={500}>contacts</Link>
+                        <a onClick={ResumeClick} href={resume_pdf} className="menuItem">cv</a>
                     </div>
                     <DropdownButton id="dropdown-basic-button" title="">
                         <Dropdown.Item><Link activeClass="active" className="menuItem" to="aboutme" spy={true} smooth={true} duration={500}>about me</Link></Dropdown.Item>
